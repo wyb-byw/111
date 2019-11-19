35172712fb211c5c33802ca6ecbf8065378d6c53
wyb-byw <2843043708@qq.com>
2019-11-19 09:14:41 +0800

    qwe

--- 119.c
+++ 119.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main(void)
 {
-    int term,sum,i,n;
+    int term,sum,i,n,fac;
     printf("Enter n:\n");
     scanf("%d",&n);
     for(sum=0,term=1,i=1;i<=n;i++)
     {
         term*=i;
         sum +=term;
+        
     }
-    printf("term=%d",term);
+    printf("sum=%d",sum);
     return 0;
 }
