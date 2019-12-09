4cfc6a75c464ad710300b95161de2af95efba08d
wyb-byw <2843043708@qq.com>
2019-12-09 22:36:49 +0800

    qwe

---
+++ 129.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+
+    printf("w        ww        w\n");
+
+    printf(" w      w  w      w\n");
+
+    printf("  w    w    w    w\n");
+
+    printf("   w  w      w  w\n");
+
+    printf("    ww        ww\n");
+
+    return 0;
+
+}
